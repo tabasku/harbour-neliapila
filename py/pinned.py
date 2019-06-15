fb6fd2da333263c444bb6084ee546733084e2916
Jacob <12831497+jacquesCedric@users.noreply.github.com>
2019-06-15 12:04:15 +1000

    New method for dealing with HTML entities
    New method for dealing with HTML entities uses html.unsescape() instead of replacing individual characters

--- py/pinned.py
+++ py/pinned.py
@@ -1,4 +1,4 @@
-__author__ = 'joni'
+__author__ = 'joni, szopin, jacques'
 
 from getdata import req
 #from threads import get_threads
@@ -6,6 +6,7 @@ import json
 import threading
 import storage
 import sys
+import html
 from utils import strip_tags,epoch_to_readable
 from posts import get_posts_info,get_new_posts_count
 from threads import get_threads_info,thread_archived
@@ -13,6 +14,7 @@ import re
 import time
 import pyotherside
 import os,sys,inspect
+
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"lib")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
@@ -27,25 +29,20 @@ def get_pins_db(post_no=None,board=None):
     pins_list=[]
 
     if post_no is None and board is None:
-
         for pin in pins:
-            short_com = pin['SHORT_COM'][2:-1].replace("&#039;","'")
-            short_com = short_com.replace("&#034;","\"")
-            short_com = short_com.replace("&#8220;","“")
-            short_com = short_com.replace("&#8221;","”")
-            short_com = short_com.replace("&#8216;","‘")
-            short_com = short_com.replace("&#8217;","’")
-            short_com = short_com.replace("&#8222;","„")
+            short_com = pin['SHORT_COM']
+            short_com = html.unescape(short_com)
+
             if len(re.findall(r'\d+s.jpg',pin['THUMB_URL'])) != 0:
                 filename = database.get_thumb_dir() + "/" + re.findall(r'\d+s.jpg',pin['THUMB_URL'])[0]
             else:
                 filename = None
+
             thread_dead = False
             archived = False
             post_count = 0
 
             now = epoch_to_readable(pin['TIME_CREATED'])
-            #last_read = epoch_to_readable(pin['TIME_CREATED'])
 
             print("GETTIng INFO BOUT {}".format(pin['POSTNO']))
 
@@ -81,19 +78,11 @@ def get_pins_db(post_no=None,board=None):
             pyotherside.send('pinned_postno', pins_list)
 
     elif board and post_no is None:
-        #pin_dict = {}
         for pin in pins:
             pins_list.append(pin['POSTNO'])
 
-        #print(pins_list)
-
-        #pins_list = dict (zip(my_dict.values(),my_dict.keys()))
         if sys.platform != 'win32':
             pyotherside.send('pinned_board', pins_list)
-
-
-
-
 
 
 def get_pinned_from_board(board):
@@ -105,23 +94,16 @@ def get_pinned_from_board(board):
 def add_pin(post_no,board,short_com,thumb_url,time_created,replies_count):
     timestamp = int(time.time())
     database = storage.Storage()
-    #Pretty ghetto way to determine if string has http tags...
-    if "<" in short_com:
-        short_com = strip_tags(short_com)
 
+    # This regex should detect any tags and strip them out.
+    tagStripper = re.compile(r'<[^>]+>')
+    short_com = tagStripper.sub('', short_com)
+
+    # limit length of pin
     if len(short_com) > 100:
         short_com = short_com[0:100]
 
-    short_com = short_com.replace("'","&#039;")
-    short_com = short_com.replace("\"","&#034;")
-    short_com = short_com.replace("“","&#8220;")
-    short_com = short_com.replace("”","&#8221;")
-    short_com = short_com.replace("‘","&#8216;")
-    short_com = short_com.replace("’","&#8217;")
-    short_com = short_com.replace("„","&#8222;")
-
-    short_com = short_com.encode()
-
+    html.escape(short_com).encode('ascii', 'xmlcharrefreplace')
 
     try:
         thumb_url
@@ -152,11 +134,9 @@ class Pinned:
             return
 
         if target_method == "get_by_postno":
-            #print("GET")
             post_no = args['postno']
             self.bgthread = threading.Thread(target=get_pins_db(post_no,None))
         elif target_method == "get_by_board":
-            #print("GET")
             board = args['board']
             self.bgthread = threading.Thread(target=get_pins_db(None,board))
         elif target_method == "get_all":
@@ -169,23 +149,4 @@ class Pinned:
         self.bgthread.start()
 
 
-#get_pins_db()
-
 data = Pinned()
-#delete_pins()
-#data.thread_this('add',{'postno':123,'board':'b'})
-#get_pins_db()
-#add_pin(34166621,'g','''I'm 5 episodes into this edgy sick dude anime and I find out they made a reboot of the entire show that is more faithful to the manga.<br><br>Is it worth it to finish the original anime or just start over with hellsing ultimate?''')
-#add_pin('667','r','''Didn't see one''')
-#data.thread_this('get_by_board',{'board':'helloworld'})
-#get_threads_info("g",50665689)
-#add_pin('0','au','''Didn't see one time''','https://t.4cdn.org/adv/1444302443728s.jpg',1444302443)
-#add_pin('16329228','adv','>fem, 19','http://i.4cdn.org/adv/1444386618975s.jpg','1444386618','100')
-#add_pin('2470783','hr','Anything about the Vietman War','http://i.4cdn.org/hr/1440155625130s.jpg','1444483661','5')
-#data.thread_this('get_all',{})
-#get_pins_db()
-#postno = postno[0]['postNo']
-#board = postno[0]['board']
-#add_pin('39894014','g','>fem, 19','http://i.4cdn.org/g/1390842451744s.jpg','39894014','100')
-#delete_pins("39894014",'g')
-#update_pin(2470783,'hr','60')
