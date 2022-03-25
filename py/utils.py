b57ca58e3b5c2eb4855cf9a19ae409c2454223a4
szopin <szopin@users.noreply.github.com>
2022-03-25 17:14:57 +0100

    Regex rework
    might be a bit too heavy

--- py/utils.py
+++ py/utils.py
@@ -39,16 +39,18 @@ def parse_posts(list,post_replies=None):
         return fixlist
 
     def parse_com(com):
-        com = html.unescape(com)
+        #com = html.unescape(com)
         #this thing still splits our urls..
-        #com = re.sub(r'\<wbr\>', '',com)
+        com = re.sub(r'&#039;', '\'',com)
+        com = re.sub(r'&quot;', '\"',com)
 
         #Here are regex for matching every uri and only tagged
-        tag = r'<a[^>]* href=\"([^\"]*)\" .*\>.*?\<\/a\>'
+        tag = r'<a[^>]* href="([^"]*)"\>.*?\<\/a\>'
+        uri = r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|games|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|moe|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|wiki|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\|\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]\|\{};:\'\".,<><>\"\'\'])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|games|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|moe|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|wiki|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))'
         ##uri = r'[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,6}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?'
-        uri = r'\b((?:[a-z][\w\-]+:(?:\/{1,3}|[a-z0-9%]\/)|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]' \
-              r'|\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{}' \
-              r';:".,<>?«»“”‘’]))'
+        #uri = r'\b((?:[a-z][\w\-]+:(?:\/{1,3}|[a-z0-9%]\/)|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]' \
+              #r'|\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{}' \
+              #r';:".,<>?«»“”‘’]))'
 
         #Fetch em boy!
         rtag = re.findall(tag,com)
@@ -61,12 +63,15 @@ def parse_posts(list,post_replies=None):
         for untagged_url in fixlist:
             fixed_url = re.sub(re.compile(uri, re.MULTILINE),r'<a href="\1">\1</a>',str(untagged_url))
             if "boards.4chan" not in untagged_url:
-                com = com.replace(untagged_url, fixed_url)
-
+                if '?' in untagged_url:
+                    untagged_url = untagged_url.replace('?', '\?')
+                if '+' in untagged_url:
+                    untagged_url = untagged_url.replace('+', '\+')
+                com = re.sub(untagged_url + r'($|\s|[^a-zA-Z0-9_/?\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf])', fixed_url + r'\1', com)
         #Make greentext green
-        com = re.sub(r'\<span class=\"quote\"\>\>(.*?)\<\/span\>', r'<font color="#32CD32">>\1</font>',com)
+        com = re.sub(r'<span class="quote">&gt;(.*?)</span>', r'<font color="#32CD32">>\1</font>',com)
         #Make spoilers blue
-        com = re.sub(r'\<s\>(.*?)\<\/s\>', r'<font color="#00CDFF">\1</font>',com)
+        com = re.sub(r'<s>(.*?)</s>', r'<font color="#00CDFF">\1</font>',com)
         return com
 
     for post in list:
